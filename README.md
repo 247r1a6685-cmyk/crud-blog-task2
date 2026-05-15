4b5d4282cf0109dcfedf15f1a5354b463411e9d4
Nagarani <247r1a6685@cmrtc.ac.in>
2026-05-15 18:30:44 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# My PHP Project
+
+PHP and MySQL project using XAMPP.
---
+++ db.php
@@ -0,0 +1,11 @@
+<?php
+
+$conn = mysqli_connect("localhost", "root", "", "student_dbs");
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+echo "Database connected successfully!";
+
+?>
